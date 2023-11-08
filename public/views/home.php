64add5ab7eedbc12a6564f6de451ecee6bca202f
Winzliu <alwin.liufandy@gmail.com>
2023-11-08 22:51:55 +0700

    menambahkan sparql

--- public/views/home.php
+++ public/views/home.php
@@ -1,3 +1,48 @@
+<?php 
+// require_once "../../vendor/autoload.php";
+require_once realpath(__DIR__ . '../../../') . "/vendor/autoload.php";
+
+// Setup some additional prefixes for DBpedia
+\EasyRdf\RdfNamespace::set('dbc', 'http://dbpedia.org/resource/Category:');
+\EasyRdf\RdfNamespace::set('dbo', 'http://dbpedia.org/ontology/');
+\EasyRdf\RdfNamespace::set('dbpedia', 'http://dbpedia.org/property/');
+\EasyRdf\RdfNamespace::set('dbr', 'http://dbpedia.org/resource/');
+\EasyRdf\RdfNamespace::set('movie', 'https://example.org/schema/movie');
+\EasyRdf\RdfNamespace::set('gold', 'http://purl.org/linguistics/gold/');
+\EasyRdf\RdfNamespace::set('dbp', 'http://dbpedia.org/property/');
+\EasyRdf\RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+\EasyRdf\RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+\EasyRdf\RdfNamespace::set('foaf', 'http://xmlns.com/foaf/0.1/');
+
+$sparql = new \EasyRdf\Sparql\Client('http://dbpedia.org/sparql');
+$sparql_jena = new \EasyRdf\Sparql\Client('http://localhost:3030/Tubes_WS/sparql');
+
+$q='SELECT DISTINCT * WHERE {' .
+    '?movie rdf:type movie:search;' .
+    '				rdfs:label ?moviename;' .
+    '				movie:abstract ?abstract;' .
+		'				movie:wiki ?wiki;'.
+		'				movie:rating ?rating;'.
+		'				movie:category ?category;' .
+		// '				movie:genre ?genre;'.
+		'				movie:country ?country'.
+    '}';
+
+  $result_all = $sparql_jena->query($q);
+
+
+
+	// foreach ($result as $row) {
+	// 	$wiki = $sparql->query(
+	// 		'SELECT DISTINCT * WHERE {' .
+	// 		'?movie rdfs:label "' . $row->moviename . '" ;'.
+	// 		' 			foaf:isPrimaryTopicOf ?wiki' .
+	// 		'}'
+	// 		);
+	// }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" class="dark">
 
@@ -20,31 +65,46 @@
     <!-- CONTENT START -->
     <main class="">
         <div class="flex flex-col">
-            <a href="./detail-movie.php">
-                <div class="w-[100vw] h-[65vh] relative">
-                    <video class="absolute top-0 -z-50 w-[100vw] brightness-50" data-uia="our-story-card-video" autoplay="" playsinline="" muted="" loop=""><source src="../img/IWTrailer.mp4" type="video/mp4"></video> 
+					<!-- movie First-->
+					<?php foreach($result_all as $res) :
+						\EasyRdf\RdfNamespace::setDefault('og');
+						$wiki= \EasyRdf\Graph::newAndLoad($res->wiki);
+						$foto_url =$wiki->image;
 
+						$genre='SELECT DISTINCT ?genre WHERE {' .
+							'?movie rdf:type movie:search;' .
+							'				rdfs:label "'. $res->moviename .'" ;' .
+							'				movie:genre ?genre;'.
+							'}';
+					
+						$result_genre = $sparql_jena->query($genre);
+						print_r($result_genre);
+					?>
+            <a href="./detail-movie.php?movie=<?= $res->moviename ?>">
+                <div class="w-[100vw] h-[65vh] relative">
                     <div class="absolute left-8 -bottom-32 z-20 flex items-end justify-between gap-8">
-                        <img src="../img/avengersHD.jpg" class="w-56 shadow-md shadow-semiBlack" alt="">
+                        <img src="<?= $foto_url ?>" class="w-56 shadow-md shadow-semiBlack" alt="">
 
                         <div class="flex flex-col gap-2 z-50">
-                            <p class="text-5xl text-[#f5f6f8] font-semibold gradient-text-light">Avengers: Infinity War</p>
+                            <p class="text-5xl text-[#f5f6f8] font-semibold gradient-text-light"><?= $res->moviename ?></p>
                             <div class="flex gap-4 gradient-text-light font-semibold">
                                 <div class="flex items-center gap-2 ">
                                     <i class="fa-solid fa-star"></i> 
-                                    <p>8.4</p>
+                                    <p><?= $res->rating ?></p>
                                 </div>
-                                <p>PG-13</p>
-                                <p>2018</p>
-                                <p>Action</p>
-                                <p>Adventure</p>
-                                <p>Sci-Fi</p>
+                                <p><?= $res->category ?></p>
+																<?php foreach($result_genre as $genre) : ?>
+                                <p><?= $genre->genre ?></p>
+																<?php endforeach?>
                             </div>
-                            <p class="text-[#f5f6f8] w-[50vw]">The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.</p>
+                            <p class="text-[#f5f6f8] w-[50vw]"><?= substr($res->abstract, 0, 250) . "..." ?></p>
                         </div>
                     </div>
                 </div>
             </a>
+						<?php break; 
+						endforeach ?>
+						<!-- movie first -->
             
             <div class="w-[100vw] h-[150px] half-gradient pl-[310px] rounded-custom"></div>
 
@@ -61,15 +121,20 @@
                             <i class="text-white text-2xl fa-solid fa-chevron-right"></i>
                         </button>
                         <div class="flex flex-nowrap lg:ml-10 md:ml-10 ml-0">
-                            <?php for ($i = 0; $i < 15; $i++) { ?>
+                            <?php foreach($result_all as $res) :
+																\EasyRdf\RdfNamespace::setDefault('og');
+																$wiki= \EasyRdf\Graph::newAndLoad($res->wiki);
+																$foto_url =$wiki->image;
+															?>
+
                             <div class="inline-block px-3">
-                                <a href="./detail-movie.php">
+                                <a href="./detail-movie.php?movie=<?= $res->moviename ?>">
                                     <div class="w-64 h-[370px] max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                                        <img src="../img/avengersHD.jpg" alt="">
+                                        <img src="<?= $foto_url ?>" alt="" width="100%">
                                     </div>
                                 </a>
                             </div>
-                            <?php } ?>
+                            <?php endforeach ?>
                         </div>
                     </div>
                 </div>
